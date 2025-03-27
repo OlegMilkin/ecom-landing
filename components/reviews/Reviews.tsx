f21e340a8aab609aa5c275175c6ce67f3f8c0e22
OlegMilkin <olegek.milkin@gmail.com>
2025-03-28 00:00:37 +0100

    slider fix

--- components/reviews/Reviews.tsx
+++ components/reviews/Reviews.tsx
@@ -17,12 +17,14 @@ export function Reviews({ reviewList }: ReviewsProps) {
 
   return (
     <div className="container" id="reviews">
-      <div className="max-w-[100%] md:max-w-[80%] mx-auto mb-5 md:mb-11">
+      <div className="max-w-[100%] md:max-w-[80%] mx-auto mb-[15px] md:mb-11">
         <h2 className='text-4xl lg:text-6xl text-center my-4 lg:my-11'>
           Customer Reviews
         </h2>
         <Carousel
+          className='max-w-[calc(100%-100px)] mx-auto md:max-w-[100%]'
           opts={{
+            loop: true,
             align: 'start',
           }}
         >
@@ -33,8 +35,8 @@ export function Reviews({ reviewList }: ReviewsProps) {
               return <ReviewItem item={item} key={id} />
             })}         
           </CarouselContent>
-          <CarouselPrevious className="bg-[url(/prev.svg)] bg-black w-[73px] h-[73px] border-none carousel-nav hidden md:block left-[-12.5%]" />
-          <CarouselNext className="bg-[url(/next.svg)] bg-black w-[73px] h-[73px] border-none carousel-nav hidden md:block right-[-12.5%]" />
+          <CarouselPrevious className="bg-none md:bg-[url(/prev.svg)] bg-black w-[35px] md:w-[73px] h-[35px] md:h-[73px] border-none carousel-nav left-[-50px] md:left-[-12.5%]" />
+          <CarouselNext className="bg-none md:bg-[url(/next.svg)] bg-black w-[35px] md:w-[73px] h-[35px] md:h-[73px] border-none carousel-nav right-[-50px] md:right-[-12.5%]" />
         </Carousel>
       </div>
     </div>
